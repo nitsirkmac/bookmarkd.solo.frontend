bb238f49c0a3160a135432dd563fbc1a0429f736
Krissy Macdonald <krissymacdonald5@gmail.com>
2023-01-04 22:18:01 -0500

    setup component tree aligned

---
+++ src/components/Bookmarks.js
@@ -0,0 +1,11 @@
+
+
+function Bookmarks() {
+    return (
+        <div>
+            <h3>Current Bookmarks</h3>
+        </div>
+    )
+}
+
+export default Bookmarks
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+
+
+function Form() {
+    return (
+        <div>
+            <h1>Add a new bookmark</h1>
+        </div>
+    )
+}
+
+export default Form
